1a6908defb4e21f5cdf515fea71d72ba0a1faaea
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-04 07:40:58 +0000

    done

---
+++ 01_basics/06_comparsion_data types.js
@@ -0,0 +1,33 @@
+/*Basic COMPARSIOn operators 
+There will be no confusuion while comparing SAME dataypes like Number,String
+*/
+// //Number to number comparsion
+// console.log(10>12);//false
+// console.log(10<12);//true
+// console.log(10>=12);//false
+// console.log(10<=12);//true
+// console.log(10==12);//false
+
+////String to string
+// console.log("10">"12");//false
+// console.log(10<"12");//true
+// console.log("hi">="12");//true
+// console.log("10"<="hello");//true
+// console.log("10"=="12");//false
+
+
+
+////comparsions to always avoid
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+
+// console.log(undefined>0);//false
+// console.log(undefined==0);//false
+// console.log(undefined>=0);//false
+
+
+
+
+
