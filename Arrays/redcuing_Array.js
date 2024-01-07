762ca3a2051f69364b9e53ed41f7fcea1b1ff775
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-07 11:13:32 +0000

    reducing array

--- Arrays/redcuing_Array.js
+++ Arrays/redcuing_Array.js
@@ -1,15 +1,25 @@
-//reducing an array
-let arr=[1,2,3,4,5,5]
+//reducing an array(sum)
+
+let list=[1,3,5,-7,9]
 let sum=0
+//using for loop
 
-//using forlllop
-for(let n of arr){
-    sum+=n
+for(items of list){
+    sum+=items
 }
-console.log(sum);
+console.log(sum);//11
 
-//using reduce() method
-const sum1=arr.reduce((accumulator,currentValue)=>accumulator+currentValue)
 
-console.log(sum1)
+//using reduce method
+/*a means "accumulator"; c means"currentValue"
+working steps:=
+a=0, c=1  => a=1
+a=1, c=3  => a=4
+a=4, c=5  => a=9
+a=9, c=-7 => a=2
+a=2, c=9  => a=11 => sum
 
+
+*/
+sum=list.reduce((a,c)=> a+c)
+console.log(sum);//11
