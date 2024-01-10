f55e7c397fead1ab11be53cae6100072e9f6596a
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-10 13:23:04 +0000

    API TALK

---
+++ objects/06_Json_API.js
@@ -0,0 +1,16 @@
+/*the APIs are generally in form Of object{},or objects inside array[{obj},{obj}]
+
+ */
+
+///example of an API of JSON dattype(ReactsJS)
+//dekh sakte hain ki hume kuch samjh aa  nhi rha bahut complicated hai
+
+//hum jsFormatter ka use krke isko understandable bana sakte bai
+
+//kcu nhi bas is API ko cpy kro,jsonformator.org per jaao or paste krke beautify/format pe click kro
+//ab humare paas different options aate hai ki  data ko kaise dekhna hai hum like Tree,Form,Text,view etc.
+
+{"results":[{"gender":"male","name":{"title":"Mr","first":"Ernesto","last":"Souza"},"location":{"street":{"number":4186,"name":"Rua Dois"},"city":"Simões Filho","state":"Maranhão","country":"Brazil","postcode":14599,"coordinates":{"latitude":"-78.3655","longitude":"-18.2849"},"timezone":{"offset":"-3:30","description":"Newfoundland"}},"email":"ernesto.souza@example.com","login":{"uuid":"5f564d50-37b1-4844-9abb-56dcae716b15","username":"silverpanda400","password":"nico","salt":"oEReFsp1","md5":"9d9d85de6ca2ca805f88f58daf7f8a7c","sha1":"e5882fda86db0654bafa55acc70fa70cf8b44a05","sha256":"498d395ace4343ac24bd92e885fa3653987ad78e2d0245e13dcc7215ef747f8e"},"dob":{"date":"1968-01-25T17:42:01.241Z","age":55},"registered":{"date":"2019-08-04T19:14:37.610Z","age":4},"phone":"(68) 4147-1320","cell":"(66) 7685-7028","id":{"name":"CPF","value":"204.769.956-56"},"picture":{"large":"https://randomuser.me/api/portraits/men/75.jpg","medium":"https://randomuser.me/api/portraits/med/men/75.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/75.jpg"},"nat":"BR"}],"info":{"seed":"e78a7a6e860aba32","results":1,"page":1,"version":"1.4"}}
+
+
+
