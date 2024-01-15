1515c6af59789296151110683b9ee4d7f3a203a3
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-15 17:36:56 +0000

    IIFE

---
+++ functions/06_IIFE.js
@@ -0,0 +1,18 @@
+//IMMEADIATELY INVOKED FUNCTION EXPRESSIONS
+//used to execute functions immeadiately without calling it seperately
+//syntax:- (function expression)(calling argurments)
+
+
+//Named IIFE(divide)
+(function divide(num1,num2){
+    console.log(num1/num2)
+})(12,6);
+//=>output: 2
+
+//use ; after each IIFE stmt to use multiple iife excecutions otherwise eroors occur
+
+// simple IIFE
+(name=>console.log(name))("bahadoor");
+//=>Bahadoor
+
+(()=>(console.log("Good Evening ")))()
