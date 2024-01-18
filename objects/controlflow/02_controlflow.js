73140b3f1ac78c539ed73517185b9c3d7fce7d22
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-18 16:47:00 +0000

    logical operators

---
+++ objects/controlflow/02_controlflow.js
@@ -0,0 +1,33 @@
+//short hand notations is used for single conditions or in a implicit manner
+const score=999
+// if(score==999) console.log("correct");
+
+
+//LOGICAL OPERATORS(&&, ||, !)
+
+//&& ,and is used to execute all the conditions must satisfied
+//||,or is used if any one condition is satisfied
+// ! not, it is opposite of true condition
+
+const isEmployeed=true
+const isStudent=true
+
+// if(isEmployeed && isStudent){
+//     console.log("the person is a studentd and a employee");
+// }
+
+
+const age=57
+// if(age>18 && age>60){
+//     console.log("person is adult and senior citizen");
+// }
+
+
+// if(age>18 || age<46){
+//     console.log("he is a men");
+// }
+
+
+// if(age!=45){//age is not equal to 45, true
+//     console.log("equal nhi hai");
+// }
