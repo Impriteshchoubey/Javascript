f17106af708d5136076f43957281787d575433e3
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-20 18:41:55 +0000

    while doq hile

---
+++ controlflow/06_while_dowhile.js
@@ -0,0 +1,38 @@
+//While loop
+// while (condition) {
+//                 }
+
+
+let index=0
+while(index<=20){
+    //console.log(`value is ${index}`);
+    index=index+2               
+}
+
+const list=["spidy","venom","amazing","capti"]
+let hero=0
+while(hero<list.length){
+    console.log(`hero is ${list[hero]}`);
+    hero++
+}
+
+
+
+
+//DO-WHILE LOOP: means pehle kaam hoga fir condition check hoga.
+//agr condition satisfy nahin hoti fir bhi ek baar kaam hoga
+
+// do {
+    
+// } while (condition);
+
+
+// let score=0
+// do{console.log(`score is ${score}`);
+// score++}while(score<=10)
+
+
+let score=12
+do{console.log(`score is ${score}`);
+score++}while(score<=10)
+//score is 12
