98724378a7eb825e273374ce90c15e7b12536259
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-20 14:14:12 +0000

    loop fro

---
+++ controlflow/04_forloop.js
@@ -0,0 +1,22 @@
+//FOR LOOP
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];}
+
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+
+//printing a table of 5
+for (let i = 1; i<=10; i++) {
+    console.log(`5 * ${i} = ${5*i}`);
+    
+}
+
+
+
+//for loop insie for loop
+//printing table of 1 to 10
+
+for(let i=)
