4941e87227bf024b88f873256207d09f132b4e21
Pritesh Chaturvedi <127940807+Impriteshchoubey@users.noreply.github.com>
2024-01-20 18:03:30 +0000

    for loop

--- controlflow/04_forloop.js
+++ controlflow/04_forloop.js
@@ -2,21 +2,40 @@
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index];}
 
-
+//printing number 1 to 10.
 // for(let i=0;i<10;i++){
 //     console.log(i);
 // }
 
 
-//printing a table of 5
-for (let i = 1; i<=10; i++) {
-    console.log(`5 * ${i} = ${5*i}`);
+//printing a multiplication table of 5
+// for (let i = 1; i<=10; i++) {
+//     console.log(`5 * ${i} = ${5*i}`);
+    
+// }
+
+
+
+//for loop inside for loop
+//printing multiplication table of 1 to 10
+
+for(let i=1;i<=10;i++){
+    console.log(`table of ${i}:`);
+    for(let j=1;j<=10;j++){
+        console.log(`${i} * ${j} = ${i*j} `);
+    }
+}
+
+
+
+
+//for loop using array
+//printing array values 
+const Myarray=["iron man","thor","spiderman","LOKI","black panther"]
+for (let i = 0; i < Myarray.length; i++) {
+    console.log(Myarray[i]);
     
 }
 
 
 
-//for loop insie for loop
-//printing table of 1 to 10
-
-for(let i=)
